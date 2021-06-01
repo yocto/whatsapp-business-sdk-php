57a431bc0401489384e3943f0e27a0f566dab5f3
Ben <ben@yocto.nu>
2021-06-01 17:04:29 +0200

    Add media edges

--- src/Edges/Media.php
+++ src/Edges/Media.php
@@ -1,12 +1,25 @@
 <?php
 namespace WHATSAPP\SDK\Edges;
 
+use WHATSAPP\SDK\API;
 use WHATSAPP\SDK\APIEdge;
+use WHATSAPP\SDK\APIRequest;
+use WHATSAPP\SDK\Edges\Media\MediaID;
 
 class Media extends APIEdge{
 
     public const ENDPOINT = '/v1/media';
 
-    //TODO
+    public function MediaID(): MediaID{
+        return new MediaID($this->getAPI(),$this->getAPI()->getEndpoint().MediaID::ENDPOINT);
+    }
+
+    public function upload(string $content_type,string $binary_media_content){
+        $request = new APIRequest(API::METHOD_POST,$this->getEndpoint(),[
+            'Authorization: Bearer '.$this->getAPI()->getToken(),
+            'Content-Type: '.$content_type,
+        ],$binary_media_content);
+        return $this->getAPI()->execute($request);
+    }
 
 }
---
+++ src/Edges/Media/MediaID.php
@@ -0,0 +1,26 @@
+<?php
+namespace WHATSAPP\SDK\Edges\Media;
+
+use WHATSAPP\SDK\API;
+use WHATSAPP\SDK\APIEdge;
+use WHATSAPP\SDK\APIRequest;
+
+class MediaID extends APIEdge{
+
+    public const ENDPOINT = '/v1/media/{media-id}';
+
+    public function retrieve(string $media_id){
+        $request = new APIRequest(API::METHOD_GET,str_replace($this->getEndpoint(),'{media-id}',$media_id),[
+            'Authorization: Bearer '.$this->getAPI()->getToken(),
+        ]);
+        return $this->getAPI()->execute($request);
+    }
+
+    public function delete(string $media_id){
+        $request = new APIRequest(API::METHOD_DELETE,str_replace($this->getEndpoint(),'{media-id}',$media_id),[
+            'Authorization: Bearer '.$this->getAPI()->getToken(),
+        ]);
+        return $this->getAPI()->execute($request);
+    }
+
+}
