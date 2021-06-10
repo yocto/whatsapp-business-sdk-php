cd5d8659e1c80e4e28d8a78e8897e00fdde59078
Ben <ben@yocto.nu>
2021-06-10 19:22:01 +0200

    Add user creation endpoint

--- src/Edges/Users.php
+++ src/Edges/Users.php
@@ -1,7 +1,9 @@
 <?php
 namespace WHATSAPP\SDK\Edges;
 
+use WHATSAPP\SDK\API;
 use WHATSAPP\SDK\APIEdge;
+use WHATSAPP\SDK\APIRequest;
 use WHATSAPP\SDK\Edges\Users\Login;
 use WHATSAPP\SDK\Edges\Users\Logout;
 use WHATSAPP\SDK\Edges\Users\ManageUsers;
@@ -20,6 +22,15 @@ class Users extends APIEdge{
 
     public function ManageUsers(): ManageUsers{
         return new ManageUsers($this->getAPI(),$this->getAPI()->getEndpoint().ManageUsers::ENDPOINT);
+    }
+
+    public function create(string $username,string $password){
+        $json = [];
+        $request = new APIRequest(API::METHOD_POST,$this->getEndpoint(),[
+            'Authorization: Basic '.base64_encode($username.':'.$password),
+            'Content-Type: application/json',
+        ],json_encode($json,JSON_FORCE_OBJECT));
+        return $this->getAPI()->execute($request);
     }
 
 }
